5cb50185dc7b62330c5941af39283300db0315c9
mvkrishna24 <marthavamshikrishna1024@gmail.com>
2026-05-01 20:28:45 +0530

    chore: add @Validated to AuthController to enable method-level constraint validation

--- src/main/java/com/vamshi/urlshortener/security/AuthController.java
+++ src/main/java/com/vamshi/urlshortener/security/AuthController.java
@@ -8,8 +8,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
