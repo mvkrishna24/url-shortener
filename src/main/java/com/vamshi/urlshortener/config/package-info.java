81eb890e5592f83e092943b209fe62534f83c77e
mvkrishna24 <marthavamshikrishna1024@gmail.com>
2026-05-01 20:29:23 +0530

    docs: add package-info.java to config package describing infrastructure bean definitions

---
+++ src/main/java/com/vamshi/urlshortener/config/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Spring {@code @Configuration} and {@code @Bean} definitions.
+ *
+ * <p>Contains infrastructure beans (JPA auditing, Redis, web MVC, security) that are
+ * split into focused config classes rather than lumped into the main application class.
+ */
+package com.vamshi.urlshortener.config;
